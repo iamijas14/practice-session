11c73354241b9db0c54940aafa20813996b3ec44
Ijas mohammed <75926327+iamijas14@users.noreply.github.com>
2021-01-26 15:24:45 +0530

    ex01-07

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
 
+var heading = "Ijas";
+var color = "red";
+var likeCounter = 0; //input
+
 export default function App() {
+  function clickHandler() {
+    likeCounter = likeCounter + 1; //proccessing
+    console.log("Liked", likeCounter); //output
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>
+        Welcome <span style={{ color: color }}>{heading}</span>
+      </h1>
+      <button onClick={clickHandler}>Like</button>
     </div>
   );
 }
