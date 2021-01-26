e78ab209d1f19513bf12fc785a32e4a283894d92
Ijas mohammed <75926327+iamijas14@users.noreply.github.com>
2021-01-26 15:56:36 +0530

    ex-till-11

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 var heading = "Ijas";
 var color = "red";
 var likeCounter = 0; //input
 
+var emojiDb = {
+  "⚽": "Soccer Ball",
+  "⚾": "Baseball",
+  "🥎": "Softball",
+  "🏀": "Basketball",
+  "🏐": "Volleyball"
+};
+
+var dataBase = Object.keys(emojiDb);
+
 export default function App() {
+  var [like, setlike] = useState();
+  var [data, setdata] = useState();
+
+  function inputHandler(ijas) {
+    console.log(ijas.target.value);
+    var userInput = ijas.target.value;
+    setdata(userInput);
+  }
   function clickHandler() {
     likeCounter = likeCounter + 1; //proccessing
     console.log("Liked", likeCounter); //output
+    setlike(likeCounter);
   }
+
   return (
     <div className="App">
       <h1>
         Welcome <span style={{ color: color }}>{heading}</span>
       </h1>
-      <button onClick={clickHandler}>Like</button>
+      <input onChange={inputHandler}></input>
+      <button onClick={clickHandler}>Like</button> {like}
+      <div>{data}</div>
+      <div>{dataBase}</div>
     </div>
   );
 }
